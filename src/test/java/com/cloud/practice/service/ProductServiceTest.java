f920cffe45373328dac8b4f115ab9d18ed12383f
kmularise <kmula222@gmail.com>
2023-11-06 14:42:17 +0900

    chore: 테스트 코드 추가(#6)

--- src/test/java/com/cloud/practice/service/ProductServiceTest.java
+++ src/test/java/com/cloud/practice/service/ProductServiceTest.java
@@ -22,62 +22,62 @@ import com.cloud.practice.repository.ProductRepository;
 import com.cloud.practice.dto.ProductAddRequest;
 import com.cloud.practice.dto.ProductAddResponse;
 
-// @SpringBootTest
+@SpringBootTest
 class ProductServiceTest {
-	// @Autowired
-	// ProductService productService;
-	// @Autowired
-	// ProductRepository productRepository;
-	//
-	// @DisplayName("낙관적 락 : 충돌 시 예외 발생")
-	// @Test
-	// void checkQuantityPositiveOrZero() throws InterruptedException, ExecutionException {
-	// 	ProductAddResponse response = productService.create(new ProductAddRequest("test", 1000L, 100L));
-	// 	List<Future<Boolean>> futures = new ArrayList<>();
-	// 	int count = 100;
-	// 	ExecutorService executorService = Executors.newFixedThreadPool(4);
-	// 	CountDownLatch countDownLatch = new CountDownLatch(count);
-	// 	for (int idx = 0; idx < count ; idx++) {
-	// 		Future<Boolean> future = executorService.submit(() -> {
-	// 			boolean failure = false;
-	// 			try {
-	// 				productService.decreaseQuantity(response.getId(), 1L);
-	// 				countDownLatch.countDown();
-	// 			} catch (ObjectOptimisticLockingFailureException e) {
-	// 				failure = true;
-	// 			} finally {
-	// 				countDownLatch.countDown();
-	// 			}
-	// 			return failure;
-	// 		});
-	// 		futures.add(future);
-	// 	}
-	// 	countDownLatch.await();
-	// 	boolean hasOptimisticLockFailure = false;
-	// 	for (Future<Boolean> hasException : futures) {
-	// 		if (hasException.get()) {
-	// 			hasOptimisticLockFailure = true;
-	// 			break ;
-	// 		}
-	// 	}
-	// 	Assertions.assertThat(hasOptimisticLockFailure).isTrue();
-	// }
+	@Autowired
+	ProductService productService;
+	@Autowired
+	ProductRepository productRepository;
 
-	// @DisplayName("낙관적 락 : 충돌 시 재시도")
-	// @Test
-	// void validWithOptimisticRetry() throws InterruptedException {
-	// 	ProductAddResponse response = productService.create(new ProductAddRequest("test", 1000L, 100L));
-	// 	int count = 100;
-	// 	ExecutorService executorService = Executors.newFixedThreadPool(4);
-	// 	CountDownLatch countDownLatch = new CountDownLatch(count);
-	// 	for (int idx = 0; idx < count ; idx++) {
-	// 		executorService.execute(() -> {
-	// 			productService.decreaseQuantityWithRetry(response.getId(), 1L);
-	// 			countDownLatch.countDown();
-	// 		});
-	// 	}
-	// 	countDownLatch.await();
-	//     Product product = productRepository.findById(response.getId()).orElseThrow(BusinessException::new);
-	// 	Assertions.assertThat(product.getStockQuantity()).isEqualTo(0L);
-	// }
+	@DisplayName("낙관적 락 : 충돌 시 예외 발생")
+	@Test
+	void checkQuantityPositiveOrZero() throws InterruptedException, ExecutionException {
+		ProductAddResponse response = productService.create(new ProductAddRequest("test", 1000L, 100L));
+		List<Future<Boolean>> futures = new ArrayList<>();
+		int count = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(4);
+		CountDownLatch countDownLatch = new CountDownLatch(count);
+		for (int idx = 0; idx < count ; idx++) {
+			Future<Boolean> future = executorService.submit(() -> {
+				boolean failure = false;
+				try {
+					productService.decreaseQuantity(response.getId(), 1L);
+					countDownLatch.countDown();
+				} catch (ObjectOptimisticLockingFailureException e) {
+					failure = true;
+				} finally {
+					countDownLatch.countDown();
+				}
+				return failure;
+			});
+			futures.add(future);
+		}
+		countDownLatch.await();
+		boolean hasOptimisticLockFailure = false;
+		for (Future<Boolean> hasException : futures) {
+			if (hasException.get()) {
+				hasOptimisticLockFailure = true;
+				break ;
+			}
+		}
+		Assertions.assertThat(hasOptimisticLockFailure).isTrue();
+	}
+
+	@DisplayName("낙관적 락 : 충돌 시 재시도")
+	@Test
+	void validWithOptimisticRetry() throws InterruptedException {
+		ProductAddResponse response = productService.create(new ProductAddRequest("test", 1000L, 100L));
+		int count = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(4);
+		CountDownLatch countDownLatch = new CountDownLatch(count);
+		for (int idx = 0; idx < count ; idx++) {
+			executorService.execute(() -> {
+				productService.decreaseQuantityWithRetry(response.getId(), 1L);
+				countDownLatch.countDown();
+			});
+		}
+		countDownLatch.await();
+	    Product product = productRepository.findById(response.getId()).orElseThrow(BusinessException::new);
+		Assertions.assertThat(product.getStockQuantity()).isEqualTo(0L);
+	}
 }
